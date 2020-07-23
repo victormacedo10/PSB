751719341f3f3f9c9a47e69fd73b12304eaf7002
Victor Macedo <victormacedo10@yahoo.com.br>
2020-07-22 21:49:28 -0300

    Update repo

--- filterbanks/N_output_h1.m
+++
@@ -1,7 +0,0 @@
-function N = N_output_h1(N_, Nh)
-	x = randn(N_, 1);
-	h = randn(Nh, 1);
-	y = conv(h, x);
-	z = downsample(y, 2);
-	N = length(z);
-end
--- filterbanks/temp.m
+++
@@ -1,7 +0,0 @@
-	if length(x) > length(y)
-		x1 = x;
-		y1 = y;
-		y1(length(x)) = 0;
-	else
-	end
-
